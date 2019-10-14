73fa4a0d5bd818e155499a9fd70c3c9d0d243c3c
unknown <valericfbg@gmail.com>
2019-10-14 09:22:22 +0300

    logical and arithmetical command

---
+++ output.asm
@@ -0,0 +1,16 @@
+@0
+M=M-1
+A=M
+D=M
+M=0
+A=A-1
+D=D-M
+@EQ.cmp.x
+D;JEQ
+@0
+A=M
+M=0
+(EQ.cmp.x)
+@0
+A=M
+M=-1
--- vmtranslator.c
+++ vmtranslator.c
@@ -2,36 +2,56 @@
 #include <string.h>
 
 #define ONE_COMMAND_CHARS_BUFFER 50
+typedef char* string;
 
-const char* PUSH_COMMAND = "push";
-const char* ARGUMENT_MEMORY_SEGMENT = "argument";
+const string PUSH_COMMAND = "push";
+const string POP_COMMAND = "pop";
+const string ADD_COMMAND = "add";
+const string SUB_COMMAND = "sub";
+const string NEG_COMMAND = "neg";
+const string AND_COMMAND = "and";
+const string OR_COMMAND = "or";
+const string NOT_COMMAND = "not";
+const string EQ_COMMAND = "eq";
+const string ARGUMENT_MEMORY_SEGMENT = "argument";
 const char ARGUMENT_ADDRESS_LOCATION = '2';
+
+int counter = 0;
+
+string handle_push_cmd(string memory_register, string offset);
+string handle_pop_cmd(string memory_register, string offset);
+string handle_add_cmd();
+string handle_sub_cmd();
+string handle_neg_cmd();
+string handle_and_cmd();
+string handle_or_cmd();
+string handle_not_cmd();
+string handle_eq_cmd();
 
 main()
 {
-    parse("test.txt");
+    FILE* input_file = fopen("test.txt", "r");
+    remove("output.asm");
+    FILE* output_file = fopen("output.asm", "a+");
+
+    parse(input_file, output_file);
+
+    fclose(input_file);
+    fclose(output_file);
 }
 
-void parse(char* file_name)
+void parse(FILE* input_file, FILE* output_file)
 {
-    FILE* file;
-
-    if (file_name != NULL)
-    {
-        file = fopen(file_name, "r");
-    }
-    else { return; }
-
     int c;
     char line_buffer[ONE_COMMAND_CHARS_BUFFER];
     char cursor;
 
-    while ((c = getc(file)) != EOF)
+    while ((c = getc(input_file)) != EOF)
     {
         if ('\n' == (char) c)
         {
             line_buffer[cursor++] = '\0';
-            split_args(line_buffer);
+            handle_cmd(line_buffer, output_file);
             memset(line_buffer, '\0', cursor + 1);
             cursor = 0;
         }
@@ -40,31 +60,128 @@ void parse(char* file_name)
             line_buffer[cursor++] = (char) c;
         }
     }
-
-    fclose(file);
 }
 
-void split_args(char* command_line)
+void handle_cmd(string command_line, FILE* output_file)
 {
-    char* command_token = strtok(command_line, " ");
+    string command_token = strtok(command_line, " ");
 
     if (command_token != NULL)
     {
+        string asm_output;
         if (strcmp(command_token, PUSH_COMMAND) == 0)
         {
-            char* memory_segment = strtok(NULL, " ");
-            char* offset = strtok(NULL, " ");
-
-            handle_push_cmd(memory_segment, offset);
+            string memory_segment = strtok(NULL, " ");
+            string offset = strtok(NULL, " ");
+            asm_output = handle_push_cmd(memory_segment, offset);
         }
+        else if (strcmp(command_token, POP_COMMAND) == 0)
+        {
+            string memory_segment = strtok(NULL, " ");
+            string offset = strtok(NULL, " ");
+            asm_output = handle_pop_cmd(memory_segment, offset);
+        }
+        else if (strcmp(command_token, ADD_COMMAND) == 0)
+        {
+            asm_output = handle_add_cmd();
+        }
+        else if (strcmp(command_token, SUB_COMMAND) == 0)
+        {
+            asm_output = handle_sub_cmd();
+        }
+        else if (strcmp(command_token, NEG_COMMAND) == 0)
+        {
+            asm_output = handle_neg_cmd();
+        }
+        else if (strcmp(command_token, AND_COMMAND) == 0)
+        {
+            asm_output = handle_and_cmd();
+        }
+        else if (strcmp(command_token, OR_COMMAND) == 0)
+        {
+            asm_output = handle_or_cmd();
+        }
+        else if (strcmp(command_token, NOT_COMMAND) == 0)
+        {
+            asm_output = handle_not_cmd();
+        }
+        else if (strcmp(command_token, EQ_COMMAND) == 0)
+        {
+            asm_output = handle_eq_cmd();
+        }
+
+        int result = fputs(asm_output, output_file);
+
+        if (result == EOF)
+        {
+            return 2;
+        }
+
+        free(asm_output);
     }
 }
 
-void handle_push_cmd(char* memory_register, char* offset)
+string handle_push_cmd(string memory_register, string offset)
 {
-    // This is not a ggreat idea because there are dynamic parts of the push command. For example, the offset calculation. Check how can we
-    char* asm_command[100];
+    string asm_command = malloc(32);
+    strcpy(asm_command, "@k\nA=M\nD=M\n@0\nM=M+1\nA=M-1\nM=D\n");
+    if (strcmp(memory_register, "local") == 0) { asm_command[1] = *offset; }
+    return asm_command;
+}
 
+string handle_pop_cmd(string memory_segment, string offset)
+{
+    string asmCmd = malloc(28);
+    strcpy(asmCmd, "@0\nM=M-1\nA=M\nD=M\n@k\nA=M\nM=D\n");
+    if (strcmp(memory_segment, "local") == 0) { asmCmd[18] = *offset; }
+    return asmCmd;
+}
 
+string handle_add_cmd()
+{
+    string asm_command = malloc(37);
+    strcpy(asm_command, "@0\nM=M-1\nA=M\nD=M\nM=0\nA=A-1\nD=D+M\nM=D\n");
+    return asm_command;
+}
 
+string handle_sub_cmd()
+{
+    string asm_command = malloc(37);
+    strcpy(asm_command, "@0\nM=M-1\nA=M\nD=M\nM=0\nA=A-1\nD=M-D\nM=D\n");
+    return asm_command;
+}
+
+string handle_neg_cmd()
+{
+    string asm_command = malloc(20);
+    strcpy(asm_command, "@0\nD=A\nA=M-1\nM=D-M\n");
+    return asm_command;
+}
+
+string handle_and_cmd()
+{
+    string asm_command = malloc(32);
+    strcpy(asm_command, "@0\nM=M-1\nA=M\nD=M\nM=0\nA=A-1\nD=D&M\n");
+    return asm_command;
+}
+
+string handle_or_cmd()
+{
+    string asm_command = malloc(32);
+    strcpy(asm_command, "@0\nM=M-1\nA=M\nD=M\nM=0\nA=A-1\nD=D|M\n");
+    return asm_command;
+}
+
+string handle_not_cmd()
+{
+    string asm_command = malloc(14);
+    strcpy(asm_command, "@0\nA=M-1\nM=!M\n");
+    return asm_command;
+}
+
+string handle_eq_cmd()
+{
+    string asm_command = malloc(83);
+    strcpy(asm_command, "@0\nM=M-1\nA=M\nD=M\nM=0\nA=A-1\nD=D-M\n@EQ.cmp.x\nD;JEQ\n@0\nA=M\nM=0\n(EQ.cmp.x)\n@0\nA=M\nM=-1\n");
+    return asm_command;
 }
